82f90e67e716e48fcd738508f11defebfb28f840
Przemyslaw Kryger <pkryger@Przemyslaws-MacBook-Air.local>
2022-12-05 17:03:15 +0000

    Add pkg file

---
+++ basic-stats-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from basic-stats.el  -*- no-byte-compile: t -*-
+(define-package "basic-stats" "20221205.165007" "Basics stats (fivenums, etc.)" 'nil :authors '(("Przemyslaw Kryger" . "pkryger@gmail.com")) :maintainer '("Przemyslaw Kryger" . "pkryger@gmail.com") :url "https://github.com/pkryger/basic-stats")
