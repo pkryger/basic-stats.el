fa15215f55c704a892e263cf0424ea015c5ed746
Przemek Kryger <pkryger@gmail.com>
2023-08-18 09:29:34 +0100

    Revert "Add pkg file"
    This reverts commit 82f90e67e716e48fcd738508f11defebfb28f840.

--- basic-stats-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from basic-stats.el  -*- no-byte-compile: t -*-
-(define-package "basic-stats" "20221205.165007" "Basics stats (fivenums, etc.)" 'nil :authors '(("Przemyslaw Kryger" . "pkryger@gmail.com")) :maintainer '("Przemyslaw Kryger" . "pkryger@gmail.com") :url "https://github.com/pkryger/basic-stats")
